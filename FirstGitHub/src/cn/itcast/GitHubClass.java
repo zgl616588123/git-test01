29c76344969f1d49edc4435bb06b1d4cb2111f83
zhangsan <zhangsan@itcast.com>
2019-03-14 18:19:42 +0800

    来了int

--- FirstGitHub/src/cn/itcast/GitHubClass.java
+++ FirstGitHub/src/cn/itcast/GitHubClass.java
@@ -4,4 +4,11 @@ public class GitHubClass {
     public static void main(String[] args) {
         System.out.println("Git来了");
     }
+
+    public void test(){
+        int i =1;
+       int c=i+1;
+        System.out.println(c);
+
+    }
 }
